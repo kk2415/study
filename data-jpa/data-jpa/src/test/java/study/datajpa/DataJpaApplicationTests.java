118f07866197d14a1c08d68790e4321497e9fb44
Kim kyunghee <kkh2415@naver.com>
2022-07-24 12:38:06 +0900

    fix : 테스트케이스 수정

--- data-jpa/data-jpa/src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -3,7 +3,7 @@ package study.datajpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DataJpaApplication.class)
 class DataJpaApplicationTests {
 
 	@Test
