0cb3ef9d965d3cd8049616de48072ffba05926a7
Kim kyunghee <kkh2415@naver.com>
2022-07-25 14:12:55 +0900

    add : 결과를 파일로 출력하기 위해 FlatFileItemWriter 구현

--- hello-spring-batch/src/main/java/com/fastcampus/hellospringbatch/job/player/FlatFileJobConfig.java
+++ hello-spring-batch/src/main/java/com/fastcampus/hellospringbatch/job/player/FlatFileJobConfig.java
@@ -4,6 +4,7 @@ import com.fastcampus.hellospringbatch.core.service.PlayerSalaryService;
 import com.fastcampus.hellospringbatch.dto.PlayerDto;
 import com.fastcampus.hellospringbatch.dto.PlayerSalaryDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -15,12 +16,21 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.adapter.ItemProcessorAdapter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineAggregator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class FlatFileJobConfig {
@@ -42,13 +52,13 @@ public class FlatFileJobConfig {
             FlatFileItemReader<PlayerDto> playerFileItemReader,
             ItemProcessor<PlayerDto, PlayerSalaryDto> playerItemProcessor,
             ItemProcessorAdapter<PlayerDto, PlayerSalaryDto> playerSalaryProcessorAdapter,
-            ItemWriter<PlayerSalaryDto> playerFileItemWriter
+            FlatFileItemWriter<PlayerSalaryDto> flatFileItemWriter
     ) {
         return stepBuilderFactory.get("flatFileStep")
                 .<PlayerDto, PlayerSalaryDto>chunk(5)
                 .reader(playerFileItemReader)
                 .processor(playerSalaryProcessorAdapter)
-                .writer(playerFileItemWriter)
+                .writer(flatFileItemWriter)
                 .build();
     }
 
@@ -86,10 +96,29 @@ public class FlatFileJobConfig {
 
     @Bean
     @StepScope
-    public ItemWriter<PlayerSalaryDto> playerFileItemWriter() {
-        return items -> {
-            items.forEach(System.out::println);
-        };
+    public FlatFileItemWriter<PlayerSalaryDto> flatFileItemWriter() throws IOException {
+        log.error("[flatFileItemWriter] start flatFileItemWriter");
+
+        //클래스를 기반으로 FieldExtractor 만들기 때문에 BeanWrapperFieldExtractor 생성
+        BeanWrapperFieldExtractor<PlayerSalaryDto> fieldExtractor = new BeanWrapperFieldExtractor<>();
+
+        // PlayerSalaryDto 멤버 변수 이름을 나열한다.
+        fieldExtractor.setNames(new String[]{"ID", "firstName", "lastName", "salary"});
+        fieldExtractor.afterPropertiesSet();
+
+        DelimitedLineAggregator<PlayerSalaryDto> lineAggregator = new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter("\t"); //델리미터를 TAP으로 지정
+        lineAggregator.setFieldExtractor(fieldExtractor);
+
+        // 기존에 파일을 덮어쓴다.
+        new File("player-salary-list.txt").createNewFile();
+        FileSystemResource resource = new FileSystemResource("player-salary-list.txt");
+
+        return new FlatFileItemWriterBuilder<PlayerSalaryDto>()
+                .name("flatFileItemWriter")
+                .resource(resource) //데이터를 입력할 리소스 지정
+                .lineAggregator(lineAggregator) //어떤 조합으로 파일에 입력할 건지
+                .build();
     }
 
 }
